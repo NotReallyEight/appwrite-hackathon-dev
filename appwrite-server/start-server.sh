593fdbdec18db295fa189b04cf269f8d0e2797c6
Eight∞ <wangzndg@gmail.com>
2022-05-18 18:58:43 +0200

    feat: server startup file

---
+++ appwrite-server/start-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo docker run -it --rm \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw \
+    --entrypoint="install" \
+    appwrite/appwrite:0.14.0
